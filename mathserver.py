134711c76ee14ee52f12e14e75bed46d03c96884
thushitharan <thushitharansutha@gmail.com>
2023-01-03 23:15:10 +0530

    First Commit

---
+++ mathserver.py
@@ -0,0 +1,44 @@
+import socket
+from subprocess import Popen, STDOUT, PIPE
+from threading import Thread
+
+class ProcessOutputThread(Thread):
+    def __init__(self, proc, conn):
+        Thread.__init__(self)
+        self.proc = proc
+        self.conn = conn
+
+    def run(self):
+        while not self.proc.stdout.closed:
+            conn.sendall(self.proc.stdout.readline())
+
+HOST = ''
+PORT = 8877
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+print("{} connected with back port {}".format(addr[0], addr[1]))
+conn.sendall("Simple Math Sever developed by Thushitharan\n\n Give any math expressions, and I will answer you :) \n\n$ ".encode())
+
+p = Popen(['bc'], stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell=True)
+output = ProcessOutputThread(p, conn)
+output.start()
+
+while not p.stdout.closed:
+    data = conn.recv(1024)
+    if not data:
+        break
+    else:
+        data = data.decode()
+        query = data.strip()
+        if query == 'quit' or query == 'exit':
+            p.communicate(query.encode(), timeout=1)
+            if p.poll() is not None:
+                break
+        query = query + '\n'
+        p.stdin.write(query.encode())
+        p.stdin.flush()
+s.close()
